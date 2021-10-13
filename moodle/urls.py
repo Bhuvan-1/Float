56de54237c370d99cf6b69961bb3851e9d3253a6
Bhuvan-1 <200050040@iitb.ac.in>
2021-10-13 18:04:36 +0530

    Signup added

--- moodle/urls.py
+++ moodle/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name = 'home'),
+    path('signup/',views.sign_up,name = 'signup'),
     path('accounts/',include('accounts.urls')),
 ]
--- moodle/views.py
+++ moodle/views.py
@@ -3,6 +3,31 @@ from django.shortcuts import render , redirect , get_object_or_404
 from django.http import HttpResponse, response
 from django.contrib.auth.models import User
 
+from django.contrib.auth.forms import UserCreationForm
+from moodle.forms import SignupForm
+import requests
+
 
 def home(request):
     return render(request,'base.html')
+
+
+
+#view corresponding to the signup page
+def sign_up(request):
+	if request.method == 'POST':
+		#form = UserCreationForm(request.POST)
+		form = SignupForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/accounts/login/')
+		else:
+			#form = UserCreationForm()
+			form = SignupForm()
+			args = { 'form': form , 'success' : False }
+			return render(request,'accounts/sign_up.html',args)
+	else:
+		#form = UserCreationForm()
+		form = SignupForm()
+		args = {'form': form , 'success' : True }
+		return render(request,'accounts/sign_up.html',args)
