d7da09785bbc2c80be9f41395ee18b4fbd9854d0
HarshadeepMoganti <90999053+HarshadeepMoganti@users.noreply.github.com>
2021-10-20 12:51:43 +0530

    Profile change

--- moodle/forms.py
+++ moodle/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserChangeForm
 from django.forms.models import ModelForm
 from courses.models import Course
 
@@ -28,3 +29,12 @@ class SignupForm(UserCreationForm):	#creating our custom registration form
 		if commit:
 			user.save()
 		return user
+	
+	class ProfileChangeForm(UserChangeForm):
+		class Meta:
+			model = User
+			fields = (
+			'email',
+			'first_name',
+			'last_name',
+			)
