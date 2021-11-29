729220555dd73f36423a3384035f414b37c17a36
Bhuvan-1 <200050040@iitb.ac.in>
2021-11-29 02:52:26 +0000

    collectstatic

--- moodle/settings.py
+++ moodle/settings.py
@@ -134,10 +134,6 @@ USE_TZ = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'accounts/static'),
-)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
